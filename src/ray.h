5a7e01ff0736d2b342cbb4f3233e48d1efde59cb
Henry Choi <electricvoxel@gmail.com>
2024-06-10 22:09:11 -0700

    remove prints

--- src/ray.h
+++ src/ray.h
@@ -14,9 +14,4 @@ public:
     const glm::vec3 &direction() const { return direction_; }
 
     glm::vec3 at(float t) const { return origin_ + direction_ * t; }
-
-    void print() const {
-        std::cout << " origin.x:" << origin_.x << " origin.y:" << origin_.y << " origin.z:" << origin_.z;
-        std::cout << " direction.x:" << direction_.x << " direction.y:" << direction_.y << " direction.z:" << direction_.z << std::endl;
-    }
 };
